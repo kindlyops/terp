ca7d4d5245a63eb6f3bc6d39e10004289720587a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-23 22:49:03 +0800

    Delete goreleaser-post-hook.ps1

--- goreleaser-post-hook.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-
-Write-Output "moving bazel outputs to goreleaser dist directory for packaging..."
-
-if (-Not (Test-Path "dist")) {
-  New-Item -Force -Path (Get-Location).Path -Name "dist" -ItemType "directory"
-}
-
-Copy-Item (Resolve-Path "bazel-bin/bdist/*") -Destination (Resolve-Path "dist") -Recurse -Force
